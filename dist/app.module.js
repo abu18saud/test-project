1154edb1c9b6de1ed727d4f7c55f12ac08a58804
Abdullah Alsalem <441226478@tvtc.edu.sa>
2022-09-08 14:47:24 +0300

    يعمل المشروع بشكل كامل وبجودة عالية

--- dist/app.module.js
+++ dist/app.module.js
@@ -10,11 +10,27 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const typeorm_1 = require("@nestjs/typeorm");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [
+            typeorm_1.TypeOrmModule.forRoot({
+                type: 'postgres',
+                host: 'ec2-34-247-72-29.eu-west-1.compute.amazonaws.com',
+                port: 5432,
+                username: 'nrrhawbkisgusi',
+                password: 'c90455b1e94aeacf88fa3f5d5b150c3ffc6ae953287f8d04b75b069386bf98bb',
+                database: 'd3ivjvcl740b8e',
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                },
+                entities: [],
+                synchronize: true,
+            }),
+        ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ec2-34-247-72-29.eu-west-1.compute.amazonaws.com',
+      port: 5432,
+      username: 'nrrhawbkisgusi',
+      password: 'c90455b1e94aeacf88fa3f5d5b150c3ffc6ae953287f8d04b75b069386bf98bb',
+      database: 'd3ivjvcl740b8e',
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false // This line will fix new error
+      },
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {
+ }
